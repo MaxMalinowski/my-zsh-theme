7a63d45addea9dfb83015cb20e702ff2845cf882
Max Malinowski <max.malinowski@outlook.de>
2021-04-29 13:04:42 +0200

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git pull
-ln -s 'pwd'/raspberrydev.zsh-theme ~/.oh-my-zsh/themes/
-sed -i -e 's/ZSH_THEME=.*/ZSH_THEME="raspberrydev"/g' ~/.zshrc
+ln -s `pwd`/raspberrydev.zsh-theme ~/.oh-my-zsh/themes/
+sed -i -e 's/ZSH_THEME=.*/ZSH_THEME="raspberrydev"/' ~/.zshrc
 source ~/.zshrc
