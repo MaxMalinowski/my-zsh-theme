352fa8c1a61afae2493259e8ca306a174b480216
Max Malinowski <max.malinowski@outlook.de>
2021-04-29 12:58:30 +0200

    adjust startup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# make sure to have oh-my-zsh installed and running
-
-ln -s ./raspberrydev.zsh-theme ~/.oh-my-zsh/themes/
+git pull
+ln -s 'pwd'/raspberrydev.zsh-theme ~/.oh-my-zsh/themes/
 sed -i -e 's/ZSH_THEME=.*/ZSH_THEME="raspberrydev"/g' ~/.zshrc
 source ~/.zshrc
