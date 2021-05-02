26d22b84ee2e2dbc843d80244bd96c43c6cfa169
RaspberryDev <max.malinowski@outlook.de>
2021-05-02 12:48:37 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 git pull
-ln -s `pwd`/raspberrydev.zsh-theme ~/.oh-my-zsh/themes/
-sed -i -e 's/ZSH_THEME=.*/ZSH_THEME="raspberrydev"/' ~/.zshrc
+ln -s `pwd`/raspberrysh.zsh-theme ~/.oh-my-zsh/themes/
+sed -i -e 's/ZSH_THEME=.*/ZSH_THEME="raspberrysh"/' ~/.zshrc
 source ~/.zshrc
